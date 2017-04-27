033f58a1eccb3d36e008a33f5561d2fb3d09b739
Seb Brookfield <seb@sbrookfield.co.uk>
2017-04-28 00:48:08 +0100

    Added ToDo javascript model and basic ListViewModel.

---
+++ ToDoManager/ToDoManager/Scripts/Models/ToDo.js
@@ -0,0 +1,11 @@
+﻿function ToDo(model) {
+
+    if (model == null)
+        model = {};
+
+    this.Id = model.Id;
+    this.Title = ko.observable(model.Title);
+    this.Detail = ko.observable(model.Detail);
+    this.DateCreated = model.DateCreated;
+    this.DateUpdated = model.DateUpdated;
+}
---
+++ ToDoManager/ToDoManager/Scripts/ViewModels/ListViewModel.js
@@ -0,0 +1,13 @@
+﻿(function() {
+    function viewModel() {
+        this.ToDos = ko.observableArray();
+    };
+
+    viewModel.LoadToDos = function(models) {
+        models.forEach(function(model) {
+            this.ToDos.push(new ToDo(model));
+        });
+    };
+
+    ko.applyBindings(viewModel);
+})();
