70da401d70c10708c2091bf5529176277671b036
Seb Brookfield <seb@sbrookfield.co.uk>
2017-04-27 21:14:52 +0100

    Initial commit of the business layer project.

---
+++ ToDoManager/BusinessLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BusinessLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BusinessLayer")]
+[assembly: AssemblyCopyright("Copyright © SBrookfield 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("63BECCA5-6CD4-47B7-A78A-C225171B9556")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
