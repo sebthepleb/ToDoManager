ade47f4c561e03932729e6ec739cd36ea084d84f
Seb Brookfield <seb@sbrookfield.co.uk>
2017-04-27 21:48:37 +0100

    Added Ioc class to aid testing when we get there, and added ToDoManager with sample data.

---
+++ ToDoManager/BusinessLayer/Framework/Ioc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Framework
+{
+    public static class Ioc
+    {
+        private static readonly Dictionary<Type, object> _instanceByType;
+
+        static Ioc()
+        {
+            _instanceByType = new Dictionary<Type, object>();
+        }
+
+        public static T Get<T>() where T : class
+        {
+            var type = typeof(T);
+
+            if (!_instanceByType.ContainsKey(type))
+                _instanceByType.Add(type, Activator.CreateInstance<T>());
+
+            return (T) _instanceByType[type];
+        }
+    }
+}
---
+++ ToDoManager/BusinessLayer/Managers/ToDoManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer.Models;
+
+namespace BusinessLayer.Managers
+{
+    public interface IToDoManager
+    {
+        IEnumerable<ToDoModel> GetAllToDos();
+        ToDoModel GetToDoById(long id);
+        ToDoModel SaveToDo(ToDoModel model);
+    }
+
+    public class ToDoManager : IToDoManager
+    {
+        public IEnumerable<ToDoModel> GetAllToDos()
+        {
+            return new[]
+            {
+                new ToDoModel
+                {
+                    Id = 1,
+                    Title = "Test 1",
+                    Detail = "I am the first test.",
+                    DateCreated = DateTime.Parse("01/01/01")
+                },
+                new ToDoModel
+                {
+                    Id = 2,
+                    Title = "Test 2",
+                    Detail = "I am the second test.",
+                    DateCreated = DateTime.Parse("02/02/02")
+                },
+            };
+        }
+
+        public ToDoModel GetToDoById(long id)
+        {
+            return new ToDoModel
+            {
+                Id = id,
+                Title = $"Test {id}",
+                Detail = $"I am test {id}.",
+                DateCreated = DateTime.Now,
+                UpdateUsername = "Hughbert.Cumberdale"
+            };
+        }
+
+        public ToDoModel SaveToDo(ToDoModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
