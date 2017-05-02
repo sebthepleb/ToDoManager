a809142efd622835d3b59b316b46b5812e7e277a
Seb Brookfield <seb@sbrookfield.co.uk>
2017-05-02 21:57:41 +0100

    Added a generic error message which displays as a modal if there isn't an error callback set for the PostToController.

--- ToDoManager/ToDoManager/Scripts/Common.js
+++ ToDoManager/ToDoManager/Scripts/Common.js
@@ -5,18 +5,36 @@
     DELETE: "DELETE"
 };
 
-var baseUrl = "http://localhost/ToDoManager/";
+var errorModalHtml = "<div id=\"uncaughtErrorModal\" class=\"modal fade\" role=\"dialog\">" +
+    "<div class=\"modal-dialog\">" +
+        "<div class=\"modal-content\">" +
+            "<div class=\"modal-body danger\">" +
+                "<i class=\"pull-right fa fa-times\" data-dismiss=\"modal\"></i>" +
+                "<h4>Oops, something went wrong.</h4>" +
+                "<p>There was an unexpected error whilst dealing with your request.</p>" +
+                "<p>Please try again, or contact us if the problem persists.</p>" +
+            "</div>" +
+        "</div>" +
+    "</div>" +
+"</div>";
+
+var genericErrorHandler = function() {
+    if (!$("#uncaughtErrorModal").length)
+        $("body").append(errorModalHtml);
+
+    $("#uncaughtErrorModal").modal("show");
+}
 
 function PostToController(options) {
     if (options == null || typeof(options) != "object")
         return;
 
     $.ajax({
-        url: baseUrl + options.url,
+        url: options.url,
         method: options.method,
         dataType: "json",
         data: options.data,
         success: options.success,
-        error: options.error
+        error: options.error || genericErrorHandler
     });
 }
